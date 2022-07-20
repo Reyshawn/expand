6d94d3f00b10dccf388b3bac47695e9fcbf48e0c
reyshawn <reshawnchang@gmail.com>
2022-07-20 16:33:01 +0800

    Add an animation to the `ExpandView` when tapping;

---
+++ expand/ExpandView.swift
@@ -0,0 +1,12 @@
+//
+//  ExpandView.swift
+//  expand
+//
+//  Created by reshawn on 2022/7/20.
+//
+
+import UIKit
+
+class ExpandView: UIView {
+  
+}
--- expand/ViewController.swift
+++ expand/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+  
+  @IBOutlet var expandViewWidthConstraint: NSLayoutConstraint!
+  @IBOutlet var expandView: ExpandView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    
+    let tap = UITapGestureRecognizer(target: self, action: #selector(onTap))
+    
+ 
+    expandView.addGestureRecognizer(tap)
+  }
+  
+  
+  @objc func onTap() {   
+    UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0, options: [.curveEaseInOut]) { [weak self] in
+      self?.expandViewWidthConstraint.constant = self?.expandViewWidthConstraint.constant == 100 ? 200 : 100
+      self?.view.layoutIfNeeded()
+    } completion: { _ in
     }
-
-
+  }
+  
 }
 
