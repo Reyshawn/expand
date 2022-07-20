087a088ba191f1afa9acd4d978cfe7caea0e0091
reyshawn <reshawnchang@gmail.com>
2022-07-20 17:12:10 +0800

    Add the shadow animation;

--- expand/ExpandView.swift
+++ expand/ExpandView.swift
@@ -9,4 +9,18 @@ import UIKit
 
 class ExpandView: UIView {
   
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    print("init:::")
+    
+    self.backgroundColor = .white
+    // self.layer.borderColor = UIColor.cyan.cgColor
+    // self.layer.borderWidth = 1
+    
+    self.layer.shadowOffset = CGSize(width: 0,
+                                      height: 5)
+    self.layer.shadowRadius = 10
+    self.layer.shadowOpacity = 0
+    self.layer.shadowColor = UIColor.black.cgColor
+  }
 }
--- expand/ViewController.swift
+++ expand/ViewController.swift
@@ -23,13 +23,26 @@ class ViewController: UIViewController {
   }
   
   
-  @objc func onTap() {   
-    UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0, options: [.curveEaseInOut]) { [weak self] in
-      self?.expandViewWidthConstraint.constant = self?.expandViewWidthConstraint.constant == 100 ? 200 : 100
+  @objc func onTap() {
+    let duration: CFTimeInterval = 0.4
+    
+    UIView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0, options: [.curveEaseInOut]) { [weak self] in
+      self?.expandViewWidthConstraint.constant = self?.expandViewWidthConstraint.constant == 50 ? 200 : 50
       self?.view.layoutIfNeeded()
     } completion: { _ in
     }
+    
+    CATransaction.begin()
+    let animation = CABasicAnimation(keyPath: "shadowOpacity")
+    animation.fromValue = self.expandView.layer.shadowOpacity
+    animation.toValue = self.expandView.layer.shadowOpacity == 0 ? 0.5 : 0
+    animation.duration = duration
+    self.expandView.layer.add(animation, forKey: animation.keyPath)
+    CATransaction.setCompletionBlock { [weak self] in
+      self?.expandView.layer.shadowOpacity = self?.expandView.layer.shadowOpacity == 0 ? 0.5 : 0
+    }
+    
+    CATransaction.commit()
   }
-  
 }
 
