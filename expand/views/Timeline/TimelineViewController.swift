9f4b6b24a12a3a000cda98889d8da637bb106f5d
Reyshawn <reshawnchang@gmail.com>
2022-07-27 23:55:24 +0800

    Use the diffable data source in the `Timeline`;

--- expand/views/Timeline/TimelineViewController.swift
+++ expand/views/Timeline/TimelineViewController.swift
@@ -9,130 +9,97 @@ import UIKit
 
 class TimelineViewController: UIViewController {
   
-  lazy var collectionView: UICollectionView = {
-    let layout = UICollectionViewCompositionalLayout { index, _ in
-      
-      let size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1 / 3))
-      let item = NSCollectionLayoutItem(layoutSize: size)
-      item.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
-      
-      let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(30))
-      
-      let group = NSCollectionLayoutGroup.vertical(layoutSize: size, subitem: item, count: 3)
-      let section = NSCollectionLayoutSection(group: group)
-      
-      section.interGroupSpacing = 100
-      
-      return section
-      // let supplementaryViews = [
-      //   NSCollectionLayoutBoundarySupplementaryItem(
-      //     layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(30)),
-      //     elementKind: UICollectionView.elementKindSectionHeader,
-      //     alignment: .top)
-      // ]
-      //
-      // switch index {
-      //   case 1:
-      //     let item = NSCollectionLayoutItem(
-      //       layoutSize: NSCollectionLayoutSize(
-      //         widthDimension: .fractionalWidth(1.0),
-      //         heightDimension: .fractionalHeight(1.0)
-      //       )
-      //     )
-      //
-      //     item.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
-      //
-      //     let group = NSCollectionLayoutGroup.horizontal(
-      //       layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.7), heightDimension: .fractionalWidth(0.7 / 1.5)),
-      //       subitems: [item]
-      //     )
-      //
-      //     let section = NSCollectionLayoutSection(group: group)
-      //     section.orthogonalScrollingBehavior = .continuous
-      //
-      //     section.boundarySupplementaryItems = supplementaryViews
-      //
-      //     return section
-      //   default:
-      //     let item = NSCollectionLayoutItem(
-      //       layoutSize: NSCollectionLayoutSize(
-      //         widthDimension: .fractionalWidth(1.0),
-      //         heightDimension: .fractionalHeight(1.0)
-      //       )
-      //     )
-      //
-      //     item.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
-      //
-      //     let group = NSCollectionLayoutGroup.horizontal(
-      //       layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.4), heightDimension: .fractionalWidth(0.4 * 1.5)),
-      //       subitems: [item]
-      //     )
-      //
-      //     let section = NSCollectionLayoutSection(group: group)
-      //     section.orthogonalScrollingBehavior = .continuous
-      //
-      //     section.boundarySupplementaryItems = supplementaryViews
-      //
-      //     return section
-      // }
-    }
-    
-    let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
-    return view
-  }()
+  enum Section {
+    case main
+  }
   
-  var collectionViewSections: Int = 3
+  var dataSource: UICollectionViewDiffableDataSource<Section, Int>! = nil
+  var collectionView: UICollectionView! = nil
   
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    view.addSubview(collectionView)
-    
-    collectionView.translatesAutoresizingMaskIntoConstraints = false
-    
-    NSLayoutConstraint.activate([
-      collectionView.topAnchor.constraint(equalTo: view.topAnchor),
-      collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-      collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-      collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-    ])
-    
-    collectionView.delegate = self
-    collectionView.dataSource = self
-    
-    collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+    navigationItem.title = "Timeline"
+    configureHierachy()
+    configureDataSource()
   }
 }
 
+
+extension TimelineViewController {
+  private func createLayout() -> UICollectionViewLayout {
+    let layout = UICollectionViewCompositionalLayout {
+      (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+      let leadingItem = NSCollectionLayoutItem(
+        layoutSize: NSCollectionLayoutSize(
+          widthDimension: .fractionalWidth(0.7),
+          heightDimension: .fractionalHeight(1.0)))
+      
+      leadingItem.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+      
+      let trailingItem = NSCollectionLayoutItem(
+        layoutSize: NSCollectionLayoutSize(
+          widthDimension: .fractionalWidth(1.0),
+          heightDimension: .fractionalHeight(0.3)))
+      
+      trailingItem.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+      
+      let trailingGroup = NSCollectionLayoutGroup.vertical(
+        layoutSize: NSCollectionLayoutSize(
+          widthDimension: .fractionalWidth(0.3), heightDimension: .fractionalHeight(1.0)),
+        subitem: trailingItem, count: 2)
+      
+      let nestedGroup = NSCollectionLayoutGroup.horizontal(
+        layoutSize: NSCollectionLayoutSize(
+          widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.4)),
+        subitems: [leadingItem, trailingGroup])
+      
+      let section = NSCollectionLayoutSection(group: nestedGroup)
+      
+      return section
+    }
+    
+    return layout
+  }
+}
+
+extension TimelineViewController {
+  private func configureHierachy() {
+    collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+    collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    collectionView.backgroundColor = .systemBackground
+    view.addSubview(collectionView)
+    collectionView.delegate = self
+  }
+  
+  private func configureDataSource() {
+    let cellRegistration = UICollectionView.CellRegistration<TextCell, Int> { (cell, indexPath, itemIdentifier) in
+      cell.label.text = "\(indexPath.section), \(indexPath.item)"
+      cell.contentView.backgroundColor = .cyan
+      cell.contentView.layer.borderColor = UIColor.black.cgColor
+      cell.contentView.layer.borderWidth = 1
+      cell.contentView.layer.cornerRadius = 8
+      cell.label.textAlignment = .center
+      cell.label.font = UIFont.preferredFont(forTextStyle: .title1)
+    }
+    
+    dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView) {
+      (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: Int) -> UICollectionViewCell? in
+      
+      return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+      
+    }
+    
+    var snapshot = NSDiffableDataSourceSnapshot<Section, Int>()
+    snapshot.appendSections([Section.main])
+    snapshot.appendItems(Array(0..<100))
+    dataSource.apply(snapshot, animatingDifferences: false)
+  }
+}
 
 extension TimelineViewController: UICollectionViewDelegate {
   func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-    if indexPath.section == 0 && indexPath.row == 0 {
-      collectionViewSections += 1
-      collectionView.reloadData()
-    }
+    collectionView.deselectItem(at: indexPath, animated: true)
   }
-}
-
-
-extension TimelineViewController: UICollectionViewDataSource {
-  func numberOfSections(in collectionView: UICollectionView) -> Int {
-    return collectionViewSections
-  }
-  
-  
-  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return 10
-  }
-  
-  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-
-    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-    cell.contentView.backgroundColor = .cyan
-    
-    return cell
-  }
-  
-
 }
